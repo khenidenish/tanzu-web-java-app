ffe6e91c48c4a246b3a1c80aca9f2e44f73aab75
khenidenish <denish.kheni@srijan.net>
2023-02-22 18:08:28 +0530

    new addition

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu! + Denish + let's plan ";
+		return "Greetings from Spring Boot + Tanzu! + Denish + let's  do this ";
 	}
 
 }
